df706160790a793757e5d7a87a3e83a08b82931a
David-sqrtpi <david.stiven911@gmail.com>
2021-03-17 13:56:16 -0500

    Initial commit

---
+++ src/test/java/com/david/application/SimpleEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
